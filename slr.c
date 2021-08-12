07926868813ce1924911bc6c2048609613dfd7b1
hadsky <88794580+hadsky@users.noreply.github.com>
2021-08-12 06:11:49 +0530

    Update slr.c

--- slr.c
+++ slr.c
@@ -1,52 +1,51 @@
 #include<stdio.h>
 #include <string.h>
 void main()
- {
-  char p[20][10];
-  char item[20];
-  int i,k,j,m,n,l,length=0,states=-1;
-  char prev,current;
-  //clrscr();
-  printf("\n Implementation of LR Predictive Parsing Technique");
-  printf("\n Enter number of productions of CFG:\n");
-  scanf("%d",&n);
-  printf("\n Enter %d productions:\n",n);
-  for(m=1;m<=n;m++)
-  {
-  scanf("%s",p[m]);
-  l=strlen(p[m]);
-  if(l>length)
-  length=l;
-  }
-  for(k=3;k<=length;k++)
-   {
+{
+	char p[20][10];
+	char item[20];
+	int i,k,j,m,n,l,length=0,states=-1;
+	char prev,current;
+  
+	printf("\n Implementation of LR Predictive Parsing Technique");
+	printf("\n Enter number of productions of CFG:\n");
+	scanf("%d",&n);
+	printf("\n Enter %d productions:\n",n);
+	for(m=1;m<=n;m++)
+	{
+		scanf("%s",p[m]);
+		l=strlen(p[m]);
+		if(l>length)
+		length=l;
+	}
+	for(k=3;k<=length;k++)
+	{
+		prev= ' ';
+		for(m=1;m<=n;m++)
+		{
+			current=p[m][k-1];
+			l=strlen(p[m]);
+			if(k<=l)
+			{
+				for(j=0,i=0;i<l;i++)
+				{
+					if(i==k)
+					item[j++]='.';
 
-  prev= ' ';
-  for(m=1;m<=n;m++)
-  {
-     current=p[m][k-1];
-     l=strlen(p[m]);
-     if(k<=l)
-   {
-     for(j=0,i=0;i<l;i++)
-    {
-    if(i==k)
-    item[j++]='.';
+					item[j++]=p[m][i];
+				}
+				if(k==l)
+					item[j++]='.';
 
-    item[j++]=p[m][i];
-     }
-      if(k==l)
-     item[j++]='.';
+				item[j]='\0';
 
-     item[j]='\0';
+				if(current != prev )
+					printf("\n    S%d: ",++states);
 
-      if(current != prev )
-      printf("\n    S%d: ",++states);
-
-      printf("\n     %s",item);
-     prev=current;
-    }
-  }
-   }
+				printf("\n     %s",item);
+				prev=current;
+			}
+		}
+	}
   
 }
